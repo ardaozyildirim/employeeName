0b25c93ff3cb84b43f12593603666ba6b75a4af2
Arda ÖZYILDIRIM <arda.ozyildirim@TRHQMLYCMD6P.local>
2024-11-12 09:09:13 +0300

    initial commit

---
+++ DAL/EmployeeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using employeeName.Models.DBEntities;
+
+
+
+namespace employeeName.DAL
+{
+	public class EmployeeDbContext : DbContext
+	{
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+	}
+}
+
---
+++ Models/DBEntities/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace employeeName.Models.DBEntities
+{
+	public class Employee
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+		public int Id { get; set; }
+		[Column(TypeName="Varchar(50DAL")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+		public DateTime DateOfBirth { get; set; }
+
+		public string Email { get; set; }
+		public double Salary { get; set; }
+    }
+}
+
